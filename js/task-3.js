c156cba3209d4003e5bc4d2906d38f4463daf0f0
Maliuk-Olexandr <maamcd100@gmail.com>
2025-04-01 14:13:23 +0300

    Add task-4.js for shipping cost calculation and update existing functions

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,16 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2
-  );
+function checkForSpam(message) {
+  // console.trace();
+  if (message.toUpperCase().includes(`SPAM`))
+    return message.toUpperCase().includes(`SPAM`);
+  else if (message.toUpperCase().includes(`SALE`))
+    return message.toUpperCase().includes(`SALE`);
+  else return message.toUpperCase().includes(`SALE`);
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
